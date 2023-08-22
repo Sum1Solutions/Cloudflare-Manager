31e3625cb00a56a4e018fb7b07231933911d1b20
Sum1Solutions <17915475+Sum1Solutions@users.noreply.github.com>
2023-08-22 13:55:36 -0400

    fixed db errors

--- app.py
+++ app.py
@@ -16,25 +16,6 @@ CLOUDFLARE_EMAIL = os.getenv('CLOUDFLARE_EMAIL')
 CLOUDFLARE_KEY = os.getenv('CLOUDFLARE_KEY')
 CLOUDFLARE_ACCOUNT_ID = os.getenv('CLOUDFLARE_ACCOUNT_ID')
 
-
-from flask import Flask, render_template, redirect, url_for
-import requests
-import os
-import logging
-import sqlite3
-
-# Initialize Flask app with custom template folder
-app = Flask(__name__, template_folder='templates')
-
-# Configure logging only when debug mode is enabled
-if app.debug:
-    logging.basicConfig(level=logging.INFO)
-
-# Load Cloudflare API credentials from environment variables
-CLOUDFLARE_EMAIL = os.getenv('CLOUDFLARE_EMAIL')
-CLOUDFLARE_KEY = os.getenv('CLOUDFLARE_KEY')
-CLOUDFLARE_ACCOUNT_ID = os.getenv('CLOUDFLARE_ACCOUNT_ID')
-
 def setup_database():
     """Initialize the database and set up tables if they don't exist."""
     conn = sqlite3.connect('cloudflare_manager.db')
@@ -57,50 +38,14 @@ def setup_database():
         )
     ''')
 
-    # Add a table to track the version of the database
+    # Create table to track the database version
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS db_version (
             version INTEGER
         )
     ''')
 
-    # Check if there's a version in the db_version table, if not, insert version 1
-    cursor.execute('SELECT COUNT(*) FROM db_version')
-    if cursor.fetchone()[0] == 0:
-        cursor.execute('INSERT INTO db_version (version) VALUES (1)')
-
-    conn.commit()
-    conn.close()
-
-    """Initialize the database and set up tables if they don't exist."""
-    conn = sqlite3.connect('cloudflare_manager.db')
-    cursor = conn.cursor()
-
-    # Create zones table if it doesn't exist
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS zones (
-            id TEXT PRIMARY KEY,
-            name TEXT,
-            status TEXT,
-            type TEXT,
-            plan_name TEXT,
-            name_servers TEXT,
-            original_name_servers TEXT,
-            created_on TEXT,
-            modified_on TEXT,
-            last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            auth_code_from_directnic TEXT
-        )
-    ''')
-
-    # Add a table to track the version of the database
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS db_version (
-            version INTEGER
-        )
-    ''')
-
-    # Check if there's a version in the db_version table, if not, insert version 1
+    # Check if there's a version in the db_version table; if not, insert version 1
     cursor.execute('SELECT COUNT(*) FROM db_version')
     if cursor.fetchone()[0] == 0:
         cursor.execute('INSERT INTO db_version (version) VALUES (1)')
@@ -109,8 +54,9 @@ def setup_database():
     conn.close()
 
 # Cloudflare API functions
+
 def get_zones_with_params(params):
-    """Fetches zones from Cloudflare based on provided parameters in a paginated manner."""
+    """Fetch zones from Cloudflare based on provided parameters in a paginated manner."""
     url = "https://api.cloudflare.com/client/v4/zones"
     headers = {
         'X-Auth-Email': CLOUDFLARE_EMAIL,
@@ -119,14 +65,12 @@ def get_zones_with_params(params):
 
     all_zones = []
     page = 0
-    total_pages = 1  # Default to 1 for the first iteration
-    max_iterations = 10  # Safety measure to avoid infinite loops
-    iteration = 0
+    total_pages = 1  # Start with 1 for the first iteration
+    max_iterations = 10  # Avoid potential infinite loops
 
-    while page < total_pages and iteration < max_iterations:
+    while page < total_pages and page < max_iterations:
         page += 1  # Increment page number
         params['page'] = page
-        iteration += 1
 
         response = requests.get(url, headers=headers, params=params)
         if response.status_code == 200:
@@ -136,22 +80,19 @@ def get_zones_with_params(params):
             # Update total_pages based on result_info from Cloudflare
             total_pages = result.get('result_info', {}).get('total_pages', 1)
         else:
-            # Log error messages only when debug mode is enabled
+            # Log error messages only in debug mode
             if app.debug:
                 logging.error(f"Failed to fetch zones for page {page}. Reason: {response.text}")
 
     return all_zones
 
-
 def get_all_zones():
     """Retrieve all zones from Cloudflare."""
     return get_zones_with_params({'per_page': 50})
 
-
 def get_paused_zones():
     """Retrieve paused zones from Cloudflare."""
     return get_zones_with_params({'status': 'paused', 'per_page': 50})
-
 
 def get_zones_without_dnssec():
     """Retrieve zones from Cloudflare where DNSSEC is not active."""
@@ -172,18 +113,19 @@ def get_zones_without_dnssec():
             if dnssec_data['result']['status'] != 'active':
                 zones_without_dnssec.append(zone)
         else:
+            # Log error messages only in debug mode
             if app.debug:
                 logging.error(f"Failed to fetch DNSSEC status for zone {zone_id}. Reason: {response.text}")
 
     return zones_without_dnssec
 
-# Application routes
+# Flask routes
+
 @app.route('/')
 def index():
     """Render the index page with a table listing all Cloudflare zones."""
     zones = get_all_zones()
     return render_template('index.html', zones=zones, title="All Zones", account_id=CLOUDFLARE_ACCOUNT_ID)
-
 
 @app.route('/pending')
 def pending():
@@ -191,20 +133,17 @@ def pending():
     pending_zones = get_zones_with_params({'status': 'pending', 'per_page': 50})
     return render_template('pending.html', zones=pending_zones, title="Pending Zones", account_id=CLOUDFLARE_ACCOUNT_ID)
 
-
 @app.route('/reactivate')
 def reactivate():
     """Render the Reactivate page with paused Cloudflare zones."""
     paused_zones = get_paused_zones()
     return render_template('reactivate.html', zones=paused_zones, title="Paused Zones", account_id=CLOUDFLARE_ACCOUNT_ID)
 
-
 @app.route('/no_dnssec')
 def no_dnssec():
     """Render a page listing Cloudflare zones without active DNSSEC."""
     zones = get_zones_without_dnssec()
     return render_template('no_dnssec.html', zones=zones, title="Zones Without DNSSEC", account_id=CLOUDFLARE_ACCOUNT_ID)
-
 
 @app.route('/save_to_db', methods=['POST'])
 def save_to_db():
@@ -252,5 +191,5 @@ def save_to_db():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    setup_database()  # Initialize the database and create tables if they don't exist.
+    setup_database()  # Initialize the database and create tables if they don't exist
     app.run(debug=True)
